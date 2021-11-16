9d420f509ec691e62d323663b90a7662db723c20
Frank Schäckermann <github-fschaeckermann@snkmail.com>
2021-11-16 08:57:41 +0100

    Removed trailing blanks in two lines

--- run_coverage_tests.sh
+++ run_coverage_tests.sh
@@ -7,14 +7,14 @@ export PYTHONPATH=/Users/de014490/Development/git/PythonSed
 
 coverage erase
 if [[ -z "$1" || "$1" == "2" ]]
-then 
+then
   echo "Activating Python 2"
   pyenv activate sed.py.2
   coverage run --source=PythonSed --timid -a -m unittest tests.coverage_unittest.unit_suite.test_unit_suite
 fi
 
 if [[ -z "$1" || "$1" == "3" ]]
-then 
+then
   echo "Activating Python 3"
   pyenv activate sed.py.3
   coverage run --source=PythonSed --timid -a -m unittest tests.coverage_unittest.unit_suite.test_unit_suite
